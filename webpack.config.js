a3f44bfad4cafe8d54ff17acd5d35d5a68916249
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-07-19 21:37:46 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
