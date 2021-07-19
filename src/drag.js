1fc318ecbdf619a3dccde194229256c88076de04
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-07-19 23:34:47 +0100

    Add getDragAfterElement and dragOver method in drag.js

--- src/drag.js
+++ src/drag.js
@@ -52,3 +52,33 @@ export const reloadContainer = (elements) => {
 
 // ----------------
 
+// Get the element just after the location of the mouse
+export const getDragAfterElement = (container, y) => {
+  const draggableElements = [...container.querySelectorAll('.item:not(.dragging)')];
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+
+    if (offset < 0 && offset > closest.offset) {
+      return { offset, element: child };
+    }
+    return closest;
+  }, { offset: Number.NEGATIVE_INFINITY }).element;
+};
+
+// Add event listener DragOver to dragzone
+export const dragOver = (container) => {
+  container.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector('.dragging');
+    if (afterElement === undefined) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
+};
+
+export { addListeners };
