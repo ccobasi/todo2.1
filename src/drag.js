4a5daa0ab4129b7bf41e0784d0030ff382cdd0cd
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-07-19 23:31:53 +0100

    Add localstorage in drag.js

--- src/drag.js
+++ src/drag.js
@@ -12,3 +12,43 @@ const addListeners = (elements) => {
     });
   });
 };
+
+/// Code for local Storage Save.
+
+export const saveLocalstorage = () => {
+  localStorage.setItem('list', JSON.stringify(list));
+};
+
+// Help save any changes in real time (Save position after DragDrop, etc...)
+//----------------------
+
+export const saveChanges = () => {
+  const newList = [];
+  const listTasks = document.querySelectorAll('.item');
+  if (listTasks.length === 0) {
+    list = [];
+    saveLocalstorage(list);
+  } else {
+    for (let i = 0; i < listTasks.length; i += 1) {
+      newList.push({
+        index: i + 1,
+        description: listTasks[i].firstChild.nextSibling.firstChild.nextSibling.value,
+        completed: listTasks[i].firstChild.nextSibling.firstChild.nextSibling.checked,
+      });
+
+      list = newList;
+      saveLocalstorage(list);
+    }
+  }
+};
+
+export const reloadContainer = (elements) => {
+  elements.forEach((item) => {
+    item.addEventListener('drop', () => {
+      saveChanges();
+    });
+  });
+};
+
+// ----------------
+
